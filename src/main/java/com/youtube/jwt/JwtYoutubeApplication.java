f80bae4f3680c50c03912f8f6a62683435cb84b1
team-learn-programming-yourself <team.learn.programming.yourself@gmail.com>
2021-05-15 14:07:37 +0530

    Setting up new spring project

---
+++ src/main/java/com/youtube/jwt/JwtYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtYoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtYoutubeApplication.class, args);
+    }
+
+}
